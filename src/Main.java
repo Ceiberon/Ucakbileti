0d224e6c655b177137b4b2c647d2526ccb08173b
Emirhan Arıgün <153736528+Ceiberon@users.noreply.github.com>
2024-03-24 11:10:26 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,26 +6,26 @@ public class Main {
 
         System.out.print("Yaşınız: ");
         Scanner input = new Scanner(System.in);
-        yas = input.nextDouble(); // nextInt() yerine nextDouble() kullanılmalı
+        yas = input.nextDouble();
 
         System.out.print("Mesafe (Km Cinsinden): ");
-        km = input.nextDouble(); // nextInt() yerine nextDouble() kullanılmalı
+        km = input.nextDouble(); 
 
         tutar = km * 0.10; // km başına ücret 0.10 TL
 
         if (yas < 12) {
-            tutar = tutar * 0.5; // 12 yaşından küçükse indirim uygula
-        } else if (yas >= 12 && yas < 24) { // '12' dahil edilmeli, '>=' kullanılmalı
-            tutar= tutar * 0.9; // 12 yaşından büyük ve 24 yaşından küçükse indirim uygula
-        } else if (yas >= 65) { // 'else if' bloğu başlangıcı hatalıydı
-            tutar = tutar * 0.7; // 65 yaşından büyükse indirim uygula
+            tutar = tutar * 0.5; 
+        } else if (yas >= 12 && yas < 24) { 
+            tutar= tutar * 0.9; 
+        } else if (yas >= 65) { 
+            tutar = tutar * 0.7;
         }
 
         System.out.print("Yolculuk Tipini Seçin (1 = Tek Yön, 2 = Gidiş Dönüş): ");
         int yolculukTipi = input.nextInt();
 
         if (yolculukTipi == 2) {
-            tutar= tutar * 1.6; // Gidiş-dönüş yapılacaksa tutara %20 indirim uygula
+            tutar= tutar * 1.6; 
         }
 
         System.out.println("Toplam Tutar: " + tutar + " TL");
